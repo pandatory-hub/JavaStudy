7b01db0a6758171185d3797cd31aa1f0ddb6cb73
pandatory <20171238@vision.hoseo.edu>
2020-12-03 00:02:43 +0900

    201203

---
+++ q13_2_2.java
@@ -0,0 +1,26 @@
+public class q13_2_2 {
+
+  public static void chaArr(int[][] arr) {
+    int[] rarr = arr[arr.length - 1];
+
+    for (int low = arr.length - 1; low > 0; low--)
+      arr[low] = arr[low - 1];
+
+    arr[0] = rarr;
+  }
+
+  public static void showcharArr(int[][] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 0; j < arr[i].length; j++)
+        System.out.print(arr[i][j] + " ");
+      System.out.println();
+    }
+  }
+
+  public static void main(String[] args) {
+    int[][] arr = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+    chaArr(arr);
+    showcharArr(arr);
+  }
+}
