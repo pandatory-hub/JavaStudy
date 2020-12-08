1146adb31d5b09203608e166bfce2b89deafae74
pandatory <20171238@vision.hoseo.edu>
2020-12-08 14:10:21 +0900

    201208

--- Q28_2_2.java
+++ Q28_2_2.java
@@ -1,10 +1,10 @@
 import java.util.function.BiFunction;
 
-class Box6<T, U> {
+class Box7<T, U> {
   private T id;
   private U con;
 
-  public Box6(T i, U c) {
+  public Box7(T i, U c) {
     id = i;
     con = c;
   }
@@ -14,11 +14,11 @@ class Box6<T, U> {
   }
 }
 
-public class Q28_2_1 {
+public class Q28_2_2 {
   public static void main(String[] args) {
-    BiFunction<Integer, String, Box6<Integer, String>> bf = (i, c) -> new Box6<>(i, c);
-    Box6<Integer, String> b1 = bf.apply(1, "Toy");
-    Box6<Integer, String> b2 = bf.apply(2, "Robot");
+    BiFunction<Integer, String, Box7<Integer, String>> bf = Box7::new;
+    Box7<Integer, String> b1 = bf.apply(1, "Toy");
+    Box7<Integer, String> b2 = bf.apply(2, "Robot");
     b1.showIt();
     b2.showIt();
   }
