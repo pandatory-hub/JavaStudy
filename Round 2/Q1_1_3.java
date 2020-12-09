af5001334eb5f7922456a9ff9c6529e660f7aff3
pandatory <20171238@vision.hoseo.edu>
2020-12-09 11:06:03 +0900

    201209

---
+++ Round 2/Q1_1_3.java
@@ -0,0 +1,9 @@
+public class Q1_1_3 {
+  public static void main(String[] args) {
+    System.out.println(12);
+    System.out.println("12");
+    System.out.println("" + 1 + 2);
+    System.out.println((10 + 2));
+    System.out.println(10 + 2);
+  }
+}
