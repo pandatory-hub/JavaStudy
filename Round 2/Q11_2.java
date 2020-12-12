aad1c7bb87c64bc084b791a4f200ccaf6e02b842
pandatory <20171238@vision.hoseo.edu>
2020-12-12 14:47:00 +0900

    201212

---
+++ Round 2/Q11_2.java
@@ -0,0 +1,7 @@
+public class Q11_2 {
+  public static void main(String[] args) {
+    StringBuilder s1 = new StringBuilder("990925-1012999");
+    s1.replace(6, 7, " ");
+    System.out.println(s1.toString());
+  }
+}
